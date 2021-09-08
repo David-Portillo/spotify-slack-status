d82ec835052b35129a16cea5167268c0db19a08e
David Portillo <david.portillo92@gmail.com>
2021-09-07 22:10:03 -0400

    moved around code

--- index.js
+++ index.js
@@ -8,10 +8,6 @@ require('dotenv').config();
 
 const app = express();
 
-const spotifyClientId = process.env.SPOTIFY_CLIENT_ID;
-const spotifyClientSecret = process.env.SPOTIFY_CLIENT_SECRET;
-const slackToken = process.env.SLACK_TOKEN;
-
 colors.setTheme({
   msg: ['grey', 'bold'],
   info: 'green',
@@ -19,6 +15,10 @@ colors.setTheme({
   error: 'red',
   listening: ['magenta', 'bold'],
 });
+
+const slackToken = process.env.SLACK_TOKEN;
+const spotifyClientId = process.env.SPOTIFY_CLIENT_ID;
+const spotifyClientSecret = process.env.SPOTIFY_CLIENT_SECRET;
 
 const port = 3001;
 const basicAuth = `Basic ${Buffer.from(spotifyClientId + ':' + spotifyClientSecret).toString('base64')}`;
@@ -64,12 +64,49 @@ const updateSlackStatus = (statusText = '', statusEmoji = '') => {
     .catch(handleAxiosException);
 };
 
-const saveSpotifyToken = (data) => {
+const monitoringSpotify = () => {
+  console.log(`-> checking for currently playing`.msg);
+  axiosSpotifyApi({
+    method: 'get',
+    url: '/v1/me/player/currently-playing',
+    headers: { Authorization: `Bearer ${getSavedSpotifyAccessToken().access_token}` },
+  })
+    .then(({ data }) => {
+      if (data === '' || !data.is_playing) {
+        console.log('-> nothing is playing'.warn);
+        updateSlackStatus();
+        setTimeout(() => monitoringSpotify(), 30000);
+      } else {
+        const currentlyPlaying = `${data.item.artists[0].name} - ${data.item.name}`;
+        spotifyMonitorTimer = data.item.duration_ms - data.progress_ms + 2000;
+        updateSlackStatus(currentlyPlaying, ':musical_note:');
+        setTimeout(() => monitoringSpotify(), spotifyMonitorTimer);
+      }
+    })
+    .catch((error) => {
+      if (error?.response.status === 401) {
+        console.log(`-> ${error.response.message}`.warn);
+        console.log(`-> refreshing access token...`.msg);
+        refreshSpotifyAccessToken();
+        console.log(`-> access token refreshed!`.info);
+        monitoringSpotify();
+      } else {
+        handleAxiosException(error);
+      }
+    });
+};
+
+const saveSpotifyAccessToken = (data) => {
   fs.writeFileSync(tokenFileName, JSON.stringify(data), (err) => {
     if (err) {
       return console.log(`-> ${err}`.error);
     }
   });
+};
+
+const getSavedSpotifyAccessToken = () => {
+  const tokenBuffer = fs.readFileSync(tokenFileName);
+  return tokenBuffer.length > 0 ? JSON.parse(tokenBuffer) : null;
 };
 
 const authorizeAccessToken = (userToken) => {
@@ -83,21 +120,16 @@ const authorizeAccessToken = (userToken) => {
   })
     .then(({ data }) => {
       console.log('-> authorized new access token, now saving it...'.msg);
-      saveSpotifyToken(data);
+      saveSpotifyAccessToken(data);
       console.log(`-> access token saved! in ${tokenFileName}`.info);
-      startMonitoringSpotify();
+      monitoringSpotify();
     })
     .catch(handleAxiosException);
 };
 
-const savedSpotifyAccessToken = () => {
-  const tokenBuffer = fs.readFileSync(tokenFileName);
-  return tokenBuffer.length > 0 ? JSON.parse(tokenBuffer) : null;
-};
-
-const getSpotifyAccessToken = () => {
+const authorizeSpotifyAccessToken = () => {
   try {
-    if (!savedSpotifyAccessToken()) {
+    if (!getSavedSpotifyAccessToken()) {
       console.log(`-> no access token found in ${tokenFileName}, attempting to authorize one`.warn);
       try {
         open(`https://accounts.spotify.com/authorize?client_id=${spotifyClientId}&response_type=code&redirect_uri=${callbackURL}&scope=user-read-currently-playing%20user-read-playback-state`);
@@ -106,64 +138,32 @@ const getSpotifyAccessToken = () => {
       }
     } else {
       console.log(`-> access token found in ${tokenFileName}, refreshing token...`.msg);
-      refreshSpotifyToken();
+      refreshSpotifyAccessToken();
       console.log('-> token refreshed!'.info);
     }
   } catch (error) {
     console.log(`-> no ${tokenFileName} file found, creating token.json file`.warn);
-    saveSpotifyToken(null);
+    saveSpotifyAccessToken(null);
     console.log(`-> ${tokenFileName} file created!`.info);
-    getSpotifyAccessToken();
+    authorizeSpotifyAccessToken();
   }
 };
 
-const refreshSpotifyToken = () => {
+const refreshSpotifyAccessToken = () => {
   axiosSpotifyAccount({
     method: 'post',
     params: {
       grant_type: 'refresh_token',
-      refresh_token: savedSpotifyAccessToken().refresh_token,
+      refresh_token: getSavedSpotifyAccessToken().refresh_token,
     },
   })
     .then(({ data }) => {
       console.log(`-> updating access token in ${tokenFileName}`.msg);
-      saveSpotifyToken({ ...savedSpotifyAccessToken(), ...data });
+      saveSpotifyAccessToken({ ...getSavedSpotifyAccessToken(), ...data });
       console.log(`-> updated access token! in ${tokenFileName}`.info);
-      startMonitoringSpotify();
+      monitoringSpotify();
     })
     .catch(handleAxiosException);
-};
-
-const startMonitoringSpotify = () => {
-  console.log(`-> checking for currently playing`.msg);
-  axiosSpotifyApi({
-    method: 'get',
-    url: '/v1/me/player/currently-playing',
-    headers: { Authorization: `Bearer ${savedSpotifyAccessToken().access_token}` },
-  })
-    .then(({ data }) => {
-      if (data === '' || !data.is_playing) {
-        console.log('-> nothing is playing'.warn);
-        updateSlackStatus();
-        setTimeout(() => startMonitoringSpotify(), 30000);
-      } else {
-        const currentlyPlaying = `${data.item.artists[0].name} - ${data.item.name}`;
-        spotifyMonitorTimer = data.item.duration_ms - data.progress_ms + 2000;
-        updateSlackStatus(currentlyPlaying, ':musical_note:');
-        setTimeout(() => startMonitoringSpotify(), spotifyMonitorTimer);
-      }
-    })
-    .catch((error) => {
-      if (error?.response.status === 401) {
-        console.log(`-> ${error.response.message}`.warn);
-        console.log(`-> refreshing access token...`.msg);
-        refreshSpotifyToken();
-        console.log(`-> access token refreshed!`.info);
-        startMonitoringSpotify();
-      } else {
-        handleAxiosException(error);
-      }
-    });
 };
 
 app.get('/callback', (req, res) => {
@@ -174,7 +174,7 @@ app.get('/callback', (req, res) => {
 
 const server = app.listen(3001, () => {
   console.log(`-> server started on port ${port} 🚀`.msg);
-  getSpotifyAccessToken();
+  authorizeSpotifyAccessToken();
 });
 
 // handle server exceptions & events
@@ -192,12 +192,13 @@ process.on('SIGTSTP', () => {
 
 process.on('SIGCONT', () => {
   console.log('-> resuming server...'.msg);
-  startMonitoringSpotify();
+  monitoringSpotify();
 });
 
 process.on('SIGINT', () => {
   console.log('-> reseting slack status before closing server...'.msg);
   updateSlackStatus();
+  console.log('-> reset slack status completed'.info)
   setTimeout(() => {
     server.close(() => process.exit(0));
   }, 1000);
